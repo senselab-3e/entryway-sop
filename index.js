bb01e34252ceba65607aa27ead279a0ba4974a5c
realrhys <rhys@relatid.com>
2018-06-11 10:01:50 -0400

    Added the randomization of files from the uploads directory.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var formidable = require('formidable'); 
+var fs = require('fs');
+var formidable = require('formidable');
+var shuffle = require('shuffle-array');
 
-// viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
@@ -25,18 +26,30 @@ app.post('/fileupload', function(req, res) {
       res.sendFile(__dirname + '/index.html');
 });
 
+function getMeRandom() {
+    var ran = Math.floor(Math.random() * Math.floor(50));
+    return Math.random() < 0.5 ? -1 * ran : ran;
+}
+
 app.get('/listuploads', function(req, res) {
-    res.json([
-        {
-            name:'Aaaa-01.jpg',
-            xdepth: 9,
-            ydepth: 12.00
-        }, {
-            name: 'Aaaa-02.jpg',
-            xdepth: -13.0,
-            ydepth: -24.00
-        }] );
+
+    fs.readdir(__dirname + '/assets/uploads/', function(err, items) {
+        console.log(items);
+        shuffle(items);
+        var rarray = items.slice(0, 3);
+        var response = [];
+        for (var i=0; i<rarray.length; i++) {
+            response.push({
+                name: rarray[i],
+                xdepth: getMeRandom(),
+                ydepth: getMeRandom()
+            });
+        }
+        res.json(response);
+    });
 });
 
 app.use(express.static('assets'))
+
+console.log("Server Listening on port http://localhost:8080.")
 app.listen(8080);
